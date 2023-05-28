a9036e14c9dbabbd3e11a27dca8593b23a7bebee
Sofiiia18 <sonasavcenko8@gmail.com>
2023-05-28 16:07:12 +0200

    added monsters and picture update

--- Aidens.Advnture.js
+++ Aidens.Advnture.js
@@ -5,18 +5,30 @@ let boardHeight = 640;
 let context;
 
 // Aiden
-let AidenWidth = 40;
-let AidenHeight =54;
-let AidenX = boardWidth/8;
+let AidenWidth = 250;
+let AidenHeight =250;
+let AidenX = boardWidth/40;
 let AidenY = boardHeight/2;
+let AidenImg;
 
 let Aiden = {
     x : AidenX,
     y : AidenY,
     width : AidenWidth,
     height : AidenHeight
-
 }
+
+//monster
+let monsterArray = [];
+let monsterWidth = 150;
+let monsterHeight = 150;
+let monsterX = boardWidth;
+let monsterY = AidenY/1;
+let monsterImg;
+
+//physics
+let velocityX = -2; //monsters moving left speed
+
 window.onload = function() {
     board = document.getElementById("board");
     board.height = boardHeight;
@@ -25,18 +37,47 @@ window.onload = function() {
 
     // draw Aiden
     //context.fillStyle = "black";
-    context.fillReact (Aiden.x, Aiden.y, Aiden.width, Aiden.height);
+    //context.fillReact (Aiden.x, Aiden.y, Aiden.width, Aiden.height);
 
     //load images
     AidenImg = new Image();
-    AidenImg.src = "./bo";
+    AidenImg.src = "./bohater.png";
     AidenImg.onload = function () {
         context.drawImage(AidenImg, Aiden.x, Aiden.y, Aiden.width, Aiden.height);
     }
 
-    //function (update) {
-    //requestAnimationFrame(update);
-    //context.clearRect(0,0, board)
-    
-    //}
+    monsterImg = new Image();
+    monsterImg.src = "./monster.png"
+
+    requestAnimationFrame(update);
+    setInterval(placeMonsters, 3500); //every 2.5 seconds
 }
+function update() {
+    requestAnimationFrame(update);
+    context.clearRect(0,0,  board.width, board.height);
+
+    //Aiden
+    context.drawImage(AidenImg,Aiden.x, Aiden.y, Aiden.width, Aiden.height);
+
+    //monster
+    for (let i = 0; i < monsterArray.length; i++) {
+        let monster = monsterArray[i];
+        monster.x += velocityX;
+        context.drawImage(monster.img, monster.x, monster.y, monster.width, monster.height);
+    }
+    }
+
+function placeMonsters() {
+    let monster = {
+        img : monsterImg,
+        x : monsterX,
+        y : monsterY,
+        width : monsterWidth,
+        height : monsterHeight,
+        passed : false
+    }
+    monsterArray.push(monster);
+}
+
+
+
